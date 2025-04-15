b3718e03a0f5323313bc1221c5dfc3a6dae70f38
Developer-Arcanox <pankajkoli.developer@gmail.com>
2025-04-15 13:27:44 +0530

    Registration completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -14,6 +16,19 @@ class UserController extends Controller
     function showRegisterForm()
     {
         return view("register");
+    }
+
+    function register(Request $request)
+    {
+        User::create([
+            "first_name" => $request->first_name,
+            "last_name" => $request->last_name,
+            "email" => $request->email,
+            "phone" => $request->phone,
+            "password" => Hash::make($request->password)
+        ]);
+
+        return redirect("/login")->with("success", "Account created successfully");
     }
 
     function showLoginForm()
--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::controller(UserController::class)->group(function () {
     Route::get('/', "index")->name("landing.page");
     Route::get("/register", "showRegisterForm")->name("register.page");
+    Route::post("/register", "register")->name("register");
     Route::get("/login", "showLoginForm")->name("login.page");
 });
